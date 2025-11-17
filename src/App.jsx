eb914addd08af51b4313606b74c190832c3c32c8
sayonaraboy2008 <barkamolbaxtiyarovich@gmail.com>
2025-11-17 18:09:15 +0500

    bos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { BrowserRouter, Routes } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <div></div>
+      <BrowserRouter>
+        <Header />
+        <Routes></Routes>
+      </BrowserRouter>
     </>
   );
 }
